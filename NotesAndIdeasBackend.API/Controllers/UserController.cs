ad41475fc128b743f990e1a9658ed7846775fd60
Sergio Camacho <sergiocamacho0113@gmail.com>
2021-09-25 15:02:29 -0500

    Create initial web api

---
+++ NotesAndIdeasBackend.API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NotesAndIdeasBackend.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Initial endpoint");
+        }
+    }
+}
